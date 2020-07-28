9c1285039467c7d551aa86a2a111ad7ecc24b040
Lawrance Towsen <lawrancetowsen@gmail.com>
2020-07-28 20:19:30 +0200

    Removed code - tested Modules

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-console.log('Hello world');
+const myModule = require('./my-module');
 
-//Reading a text file and displaying output
-const { readFile, readFileSync } = require('fs');
-
-const txt = readFileSync('./hello.txt', 'utf8');
-console.log(txt);
-
-console.log('do this ASAP');
-
-//Reading a textfile without blocking
-readFile('./hello.txt', 'utf8', (err,txt) =>{ // only runs inside code once the file is read in, also captures if error occurs while reading file in
-    console.log(txt);
-})
-console.log(txt);
-console.log('do this ASAP');
-
-//Reading files with promise based solution
-const { readFiles } = require('fs').promises;
-
-async function hello(){
-    const file = await readFiles('./hello.txt', utf8);
-}
-
-//How to create an EventEmitter
-const { EventEmitter } = require('events');
-const eventEmitter = new EventEmitter();
-
-eventEmitter.on('lunch', () => {
-    console.log('yum')
-})
-
-eventEmitter.emit('lunch');
-eventEmitter.emit('lunch');
-
+console.log(myModule);
