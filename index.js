8769d68d06bd93099aeee916a5f1b651cdeea8dc
Lawrance Towsen <lawrancetowsen@gmail.com>
2020-07-28 20:32:48 +0200

    Added HTML file and basic output

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const myModule = require('./my-module');
+const express = require('express');
+const { readFile } = require('fs');
 
-console.log(myModule);
+const app = express();
+
+app.get('/', (request, response) => {
+
+    readFile('./home.html', 'utf8', (err, html) => {
+        if(err){
+            response.status(500).send('sorry, out of order')
+        }
+        
+        response.send(html);
+    })
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('App available on http://localhost:3000'));
